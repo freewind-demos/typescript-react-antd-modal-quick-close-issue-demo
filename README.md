da34f3302bf1b7a9fdfed3c362c697b8e24fc11e
Freewind <nowindlee@gmail.com>
2025-04-24 00:00:23 +0800

    typescript-react-antd-modal-quick-close-issue-demo

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-TypeScript React AntDesign Modal Demo
+TypeScript React Antd Modal Quick Close Issue Demo
 =================================
 
-注意：html中顶部声明`<!DOCTYPE html>`一定要有，否则Modal右上角的x位置会有错误
+当使用ahooks的 useRequest中的loading来控制Modal的open时，如果抛出异常导致loading快速变化，则点击几次后，页面上的按钮将不可点击，似乎有一些透明的modal在上面挡住了整个页面。
+
+When using the `loading` in `useRequest` of `ahooks` to control the `open` state of a `Modal`, if an exception is thrown causing the `loading` to change rapidly, after clicking several times, the buttons on the page will become unclickable. It seems that there is some transparent `Modal` on top, blocking the entire page.  
 
 ```
 npm install
-npm run demo
+npm start
 ```
 
 It will open page on browser automatically.
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,25 +1,43 @@
-import { Button, Modal } from 'antd';
-import React, { useState } from 'react';
+import { useRequest } from 'ahooks';
+import { Button, Flex, Modal } from 'antd';
+import React, { useEffect, useState } from 'react';
 
 export default function Hello() {
   const [open, setOpen] = useState(false);
+
+  const { run, loading } = useRequest(async () => {
+    return new Error('error');
+  }, {
+    manual: true,
+  });
+
+  useEffect(() => {
+    if (loading) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }, [loading]);
+
   return <div>
-    <Button type="primary" onClick={() => setOpen(true)}>
-      Open Modal
-    </Button>
+    <Flex justify="center" align="center" style={{ height: '100vh' }} vertical>
+      <Button type="primary" onClick={run}>
+        Click me
+      </Button>
+      <div>
+        <p>Click the button above to open and close modal quickly several times, </p>
+        <p>the button will not be clickable anymore,</p>
+        <p>seems like there is a transparent modal on the top of the page</p>
+      </div>
+    </Flex>
     <Modal
       open={open}
+      closable={false}
       title="Basic Modal"
-      onOk={() => {
-        console.log("### onOk")
-        setOpen(false)
-      }}
-      onCancel={() => {
-        console.log("### onCancel")
-        setOpen(false)
-      }}
+      footer={null}
+      animation={false}
     >
-      <p>Some content</p>
+      <p>Loading ...</p>
     </Modal>
   </div>
 };
